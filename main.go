8a8fa017b62bc8410e6ea82b68f5d28405250cfe
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-11-14 16:24:53 -0300

    feat: allow to return an error
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- main.go
+++ main.go
@@ -13,13 +13,17 @@ import (
 var boldStyle = lipgloss.NewStyle().Bold(true)
 
 // Liner takes an item and returns the values its columns should have.
-type Liner[T any] func(t T) []string
+type Liner[T any] func(t T) ([]string, error)
 
 // Render renders the table to the given io.Writer.
 func Render[T any](w io.Writer, items []T, columns []string, liner Liner[T]) error {
 	tw := newTabWriter(w, columns...)
 	for _, item := range items {
-		fmt.Fprintln(tw, strings.Join(liner(item), "\t"))
+		line, err := liner(item)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(tw, strings.Join(line, "\t"))
 	}
 	if len(items) == 0 {
 		fmt.Fprintln(tw, "No items found")
@@ -39,4 +43,3 @@ func newTabWriter(w io.Writer, columns ...string) *tabwriter.Writer {
 	}
 	return tw
 }
-
--- main_test.go
+++ main_test.go
@@ -1,14 +1,15 @@
 package tablewriter
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"testing"
 )
 
 func TestRenderEmpty(t *testing.T) {
-	if err := Render(os.Stderr, nil, []string{"Item"}, func(item string) []string {
-		return []string{"doesnt matter"}
+	if err := Render(os.Stderr, nil, []string{"Item"}, func(item string) ([]string, error) {
+		return []string{"doesnt matter"}, nil
 	}); err != nil {
 		t.Fatal("render failed:", err)
 	}
@@ -24,9 +25,21 @@ func TestRender(t *testing.T) {
 		{"Carlos", 32},
 		{"John", 41},
 		{"Jose", 28},
-	}, []string{"Name", "Age"}, func(item Person) []string {
-		return []string{item.Name, strconv.Itoa(item.Age)}
+	}, []string{"Name", "Age"}, func(item Person) ([]string, error) {
+		return []string{item.Name, strconv.Itoa(item.Age)}, nil
 	}); err != nil {
 		t.Fatal("render failed:", err)
+	}
+}
+
+func TestRenderError(t *testing.T) {
+	if err := Render(os.Stderr, []Person{
+		{"Carlos", 32},
+		{"John", 41},
+		{"Jose", 28},
+	}, []string{"Name", "Age"}, func(item Person) ([]string, error) {
+		return nil, fmt.Errorf("foo bar")
+	}); err == nil {
+		t.Fatal("expected render to fail, got nil")
 	}
 }
